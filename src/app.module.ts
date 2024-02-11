a1c3c5adcc399060ee96664885df8190e53addf0
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-11 13:01:09 +0100

    add reservation module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import {ConfigModule } from "@nestjs/config"
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [MoviesModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI), ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })
