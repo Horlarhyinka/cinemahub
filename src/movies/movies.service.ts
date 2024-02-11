3e239a7bc5f03ec3c79038ed28dedcaaf56fd3c8
Horlarhyinka <danijufarouq2003@gmail.com>
2024-02-11 15:13:50 +0100

    setup movie service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -3,13 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Movie } from './movies.schema';
 import { Model } from 'mongoose';
 import { CreateMovieDto } from './dto/create-movie-dto';
+import { UpdateMovieDto } from './dto/update-movie-dto';
 
 @Injectable()
 export class MoviesService {
     constructor(@InjectModel(Movie.name) private MovieModel: Model<Movie>){}
     async CreateMovie(CreateMovieDto: CreateMovieDto){
         try{
-
         const NewMovie = await this.MovieModel.create({...CreateMovieDto, Date: new Date(CreateMovieDto.Date)})
         return NewMovie
         }catch(err){
@@ -27,6 +27,25 @@ export class MoviesService {
     async GetMovie(movieId: string ){
         const movie = await this.MovieModel.findById(movieId)
         if(!movie)throw Error("Movie not found")
+        return movie
+    }
+
+    async UpdateMovie(movieId: string, UpdateMovieDto: UpdateMovieDto){
+        try{
+
+        const updated = await this.MovieModel.findByIdAndUpdate(movieId, UpdateMovieDto, {new: true})
+            return updated
+
+        }catch(err){
+            throw err
+        }
+    }
+
+    async PopTicket(movieId: string, amount: number){
+        const movie = await this.MovieModel.findById(movieId)
+        if(!movie)throw Error("movie not found")
+        movie.NumberOfTickets -= amount
+        await movie.save()
         return movie
     }
 }
